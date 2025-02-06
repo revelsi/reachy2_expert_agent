3d32f8fa06f60691b2554b7a668f9fcb06f2ae0f
revelsi <69704153+revelsi@users.noreply.github.com>
2025-02-06 20:41:51 +0100

    cleaned up repos

--- test_file.py
+++
@@ -1,8 +0,0 @@
-lines = []
-for i in range(1, 151):
-    lines.append(f"print('This is test line number {i}: Lorem ipsum dolor sit amet, consectetur adipiscing elit.')\n")
-
-with open(__file__, 'a') as f:
-    f.write(''.join(lines))
-
-# End of test file. 
